97f154f8fff000d42b955358f3cb1611b65be466
karaografi <karayahya84@gmail.com>
2023-03-26 00:05:33 +0300

    switch-case c#

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int month = DateTime.Now.Month;
+
+
+        //Expression Kontrol Edilecek alan
+        switch (month)
+        {
+            case 1:
+                Console.WriteLine("Ocak Ayındayız");
+                break;
+            case 2:
+                Console.WriteLine("Şubat Ayındayız");
+                break;
+            case 3:
+                Console.WriteLine("Mart Ayındayız");
+                break;
+            case 4:
+                Console.WriteLine("Nisan Ayındayız");
+                break;
+            case 5:
+                Console.WriteLine("Mayıs Ayındayız");
+                break;
+            default:
+                Console.WriteLine("Yanlış Veri Girişi");
+                break;
+        }
+
+        switch (month)
+        {
+            case 12:
+            case 1:
+            case 2:
+                Console.WriteLine("Kış ayındayız");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                Console.WriteLine("İlkbahar Ayındayız");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                Console.WriteLine("Yaz Ayındayız");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                Console.WriteLine("Sonbahar Ayındayız");
+                break;
+            default:
+            break;
+        }
+    }
+}
